3d46282f1d4e2576dabd79134b49388152c64619
Linoy345 <linoysela345@gmail.com>
2021-06-16 01:14:03 +0300

    view is working, need to add function and connect the joystick to the main activity

---
+++ app/src/main/java/com/example/myapplication/Joystick.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.graphics.Canvas
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+
+class Joystick(context: Context?) : View(context) {
+
+
+
+}
