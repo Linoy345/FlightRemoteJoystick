9f555e6b96cf3498fa37cbf1adf0328ec6b1db5b
yair2121 <77067231+yair2121@users.noreply.github.com>
2021-06-17 14:55:05 +0300

    Added Client, still need to test it.

---
+++ app/src/main/java/model/Client.kt
@@ -0,0 +1,35 @@
+package model
+
+import java.io.OutputStream
+import java.net.InetSocketAddress
+import java.net.Socket
+
+//TODO: Test this class.
+class Client() {
+    private  var client: Socket = Socket()
+    private lateinit var writer: OutputStream
+
+    constructor(ip: String, port: Int) : this() {
+            client.connect(InetSocketAddress(ip, port))
+            writer = client.getOutputStream()
+    }
+    private fun isConnected() :Boolean{
+        return client.isConnected
+    }
+    fun connect(ip: String, port: Int){
+        if(!isConnected()) {
+            client.connect(InetSocketAddress(ip, port))
+            writer = client.getOutputStream()
+        }
+    }
+    fun close(){
+        client.close()
+    }
+    fun write(message: String){
+        if(this.isConnected()) {
+            val charset = Charsets.US_ASCII
+            writer.write(message.toByteArray(charset))
+        }
+    }
+}
+
