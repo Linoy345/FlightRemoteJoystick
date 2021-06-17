c6d699dd47d3b79e52cae5c5d23a1f5c21f8ccc3
Linoy345 <linoysela345@gmail.com>
2021-06-18 02:47:28 +0300

    add eli setting from video

--- app/src/main/java/com/example/myapplication/views/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/views/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.views
 
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -8,27 +9,48 @@ import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
 
+// variable that we shall initialize at a later point in code
+    lateinit var ip:EditText
+    lateinit var port:EditText
+    lateinit var buttonConnect:Button
+    //lateinit var joystick: Joystick
+    lateinit var rudder:SeekBar
+    lateinit var throttle:SeekBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //joystick.setOnClickListener()
         //val joystick = Joystick(this)
-       // setContentView(R.layout.joystick)
+        //setContentView(R.layout.joystick)
+        //val joystick = findViewById<Joystick>(R.id.allJoystick);
+        //val canvas = Canvas()
+        //joystick.draw(canvas);
+        ip = findViewById(R.id.ipText);
+        port = findViewById(R.id.portText);
+        buttonConnect = findViewById(R.id.ConnectButton);
+        rudder = findViewById(R.id.rudder);
+        throttle = findViewById(R.id.throttle);
 
-        val ip = findViewById<EditText>(R.id.ipText);
-
-        val port = findViewById<EditText>(R.id.portText);
-
-        val buttonConnect = findViewById<TextView>(R.id.ConnectButton);
         buttonConnect.setOnClickListener {
             port.text.toString(); // todo send to vm to start
             ip.text.toString(); // todo send to vm
         }
 
-        val rudder = findViewById<SeekBar>(R.id.rudder);
-        val throttle = findViewById<SeekBar>(R.id.throttle);
 
-        //rudder. todo update from vm
-        //throttle. todo update from vm
+        rudder.setOnClickListener(){        //rudder. todo update vm
+
+
+        }
+        throttle.setOnClickListener(){ //throttle. todo update vm
+
+        }
+       // joystick.onTouchEvent() //todo get the new param
+
+        //joystick.onchange=(a,e) {//todo send the param to vm
+            //vm.setAilron(a)
+            //vm.setElevator(e)
+        //}
 
     }
 
