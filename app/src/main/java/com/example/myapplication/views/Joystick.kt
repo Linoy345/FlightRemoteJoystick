aa01be1037d564b78ab4ec13d7266db1399600ad
Linoy345 <linoysela345@gmail.com>
2021-06-19 00:46:30 +0300

    set the joystick im the mainactivity

--- app/src/main/java/com/example/myapplication/views/Joystick.kt
+++ app/src/main/java/com/example/myapplication/views/Joystick.kt
@@ -2,32 +2,54 @@ package com.example.myapplication.views
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 
-class Joystick(context: Context?) : View(context) {
+class Joystick : View {
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+    }
+
+    private fun init(context: Context) {
+        //do stuff that was in your original constructor...
+    }
+
     //todo set the joystick state according to aileron+elevator
 
+    var aileron:Float = 500.0F
+    var elevator:Float = 400F
+    var radius:Float = 300F
+
+    private fun setVal(newAil:Float, newElev:Float) {
+        this.aileron = newAil
+        this.elevator = newElev
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+
+        var paint = Paint()
+        paint.color = Color.BLACK
+        canvas.drawCircle(aileron, elevator, radius, paint)
 
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
 
-        /*override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-            when (event?.action) {
-                MotionEvent.ACTION_DOWN -> //Do Something
-            }
-
-            return v?.onTouchEvent(event) ?: true
-        }*/
         val eventAction = event.action
-        // you may need the x/y location
-        val x = event.x.toDouble() //todo send as rudder to vm
-        val y = event.y.toDouble() //todo send as throttle to vm
-
+        val x = event.x //todo send as rudder to vm
+        val y = event.y //todo send as throttle to vm
+        setVal(x,y)
         return true;
     }
 
