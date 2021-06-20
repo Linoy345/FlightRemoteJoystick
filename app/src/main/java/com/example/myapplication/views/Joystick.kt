e92320f5f33889d8809f6cc79c77f6aa4d5ff954
Linoy345 <linoysela345@gmail.com>
2021-06-20 23:15:48 +0300

    joystick and sliders works

--- app/src/main/java/com/example/myapplication/views/Joystick.kt
+++ app/src/main/java/com/example/myapplication/views/Joystick.kt
@@ -13,9 +13,10 @@ import kotlin.math.sqrt
 class Joystick : View {
     private var centerX:Float = 0F
     private var centerY:Float = 0F
+    private var changeX:Float = 0F
+    private var changeY:Float = 0F
     private var bigRadius:Float = 0F
     private var littleRadius:Float = 0F
-//    lateinit var canvas: Canvas
 
     fun interface JoystickListener {
        fun onChange(x: Float, y: Float) : Unit
@@ -27,8 +28,8 @@ class Joystick : View {
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-
     }
+
 
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
     }
@@ -36,122 +37,55 @@ class Joystick : View {
     private fun setup(width:Float, height:Float) {
         this.centerX = (width / 2)
         this.centerY = (height / 2)
-        this.bigRadius = (min(width, height) / 2)
-        this.littleRadius = (min(width, height) / 6)
+        if(this.changeX == 0F){
+            this.changeX = (width / 2)
+        }
+        if(this.changeY == 0F){
+            this.changeY = (height / 2)
+        }
+        this.bigRadius = (min(width, height)) / 3
+        this.littleRadius = (min(width, height)) / 7
     }
 
-    private fun drawJoystick(x:Float, y:Float) {
-        var paint = Paint()
-        paint.color = Color.GRAY
-    }
 
     override fun onDraw(canvas: Canvas) {
-//        this.canvas = canvas
-        super.onDraw(canvas)
-        var paint = Paint()
-        paint.color = Color.BLACK
         setup(width.toFloat(), height.toFloat())
+        super.onDraw(canvas)
+        val paint = Paint()
+        paint.color = Color.BLACK
         canvas.drawCircle(centerX, centerY, bigRadius, paint)
         paint.color = Color.WHITE
-        canvas.drawCircle(x, y, littleRadius, paint)
-    }
-
-    private fun updateValues(x:Float, y:Float) {
-        this.centerX = x
-        this.centerY = y
-    }
-
-    fun getcenterX():Float {
-        return this.centerX
-    }
-    fun getcenterY():Float {
-        return this.centerY
-    }
-
-    private fun checkValidMove(x:Float, y:Float) {
-        val distance =
-            sqrt((x - centerX).toDouble().pow(2.0) + (y - centerY).toDouble().pow(2.0))
-                .toFloat()
-        if(distance < bigRadius) { //valid
-            updateValues(x, y)
-            drawJoystick(x, y)
-            service.onChange(x,y)
-        } else { //invalid - constrain the joystick to the big radius
-            val ratio: Float = bigRadius / distance
-            val constrainedX: Float = centerX + (x - centerX) * ratio
-            val constrainedY: Float = centerY + (y - centerY) * ratio
-            updateValues(constrainedX, constrainedY)
-            drawJoystick(constrainedX, constrainedY)
-            service.onChange(constrainedX,constrainedY)
-        }
-    }
-
-
-    override fun performClick(): Boolean {
-        // do what you want
-        return true
+        canvas.drawCircle(changeX, changeY, littleRadius, paint)
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         if(event == null) {
             return true;
         }
-        if(event.action != MotionEvent.ACTION_UP) {
+        if(event.getActionMasked() == MotionEvent.ACTION_DOWN ||
+            event.getActionMasked() == MotionEvent.ACTION_MOVE)  {
             val x = event.x
             val y = event.y
             val distance =
                 sqrt((x - centerX).toDouble().pow(2.0) + (y - centerY).toDouble().pow(2.0))
                     .toFloat()
             if(distance < bigRadius) { //valid
-//                updateValues(x, y)
-//                drawJoystick(x, y)
-                service.onChange(x,y)
-            } else { //invalid - constrain the joystick to the big radius
-                val ratio: Float = bigRadius / distance
-                val constrainedX: Float = centerX + (x - centerX) * ratio
-                val constrainedY: Float = centerY + (y - centerY) * ratio
-//                updateValues(constrainedX, constrainedY)
-//                drawJoystick(constrainedX, constrainedY)
-                service.onChange(constrainedX,constrainedY)
-                invalidate()
-            }
+                changeX = x
+                changeY = y
+                service.onChange(changeX,changeY)
 
-        } else {
-            //val x = event.x
-            //val y = event.y
-           // updateValues(x, y)
-           // drawJoystick(x, y)
-           // service.onChange(x,y)
+            } else { //invalid - constrain the joystick to the big radius
+                val ratio = bigRadius / distance
+                val constrainedX = centerX + (x - centerX) * ratio
+                val constrainedY = centerY + (y - centerY) * ratio
+                changeX = constrainedX
+                changeY = constrainedY
+                service.onChange(constrainedX,constrainedY)
+            }
         }
+        invalidate()
         return true;
     }
 
 
-    /*override fun onTouchEvent(event: MotionEvent): Boolean {
-        if(event == null) {
-            return true;
-        }
-        when (event?.action) {
-            MotionEvent.ACTION_DOWN-> {
-                checkValidMove(event.x, event.y)
-            }
-        }
-        return true;
-    }*/
-
-    /*public fun move(event: MotionEvent) {
-            when (event.actionMasked) {
-                MotionEvent.ACTION_DOWN-> {
-                    checkValidMove(event.x, event.y)
-                }
-                MotionEvent.ACTION_UP-> {
-                    checkValidMove(event.x, event.y)
-                }
-                MotionEvent.ACTION_MOVE-> {
-                    checkValidMove(event.x, event.y)
-                }
-
-            }
-
-    }*/
 }
